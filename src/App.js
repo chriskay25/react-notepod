dc70fcee834bf4939d6e7c9ec0ec78654efadcc9
Chris Kay <ckay84@protonmail.com>
2021-01-02 16:02:35 -0500

    Add a Home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Podcasts from './components/Podcasts'
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
+      <Home />
       <Podcasts />
     </div>
   );
