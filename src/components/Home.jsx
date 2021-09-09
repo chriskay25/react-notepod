f702f11c9681e420e059abedf036789bb1a14b4c
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-09-08 23:58:57 -0400

    Add link for podcasts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div>
             <h1>WELCOME TO THE NOTEPOD APP!</h1>
+            <Link to='/podcasts'>View Podcasts</Link>
         </div>
     )
 }
