91ad0edc9c20295ed8151fd7d92349c2398a8f30
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-09-08 23:56:05 -0400

    Populate podcast with image and title, include motion to use in future

--- src/components/Podcast.jsx
+++ src/components/Podcast.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
-const Podcast = ({ id, name }) => {
-  return (
-    <div>
-      {name}
-    </div>
-  )
+const Podcast = ({ data }) => {
+    return (
+        <motion.div className='podcast' whileHover={{scale: 1.1}} transition={{ duration: 1 }}>
+          <div className='podcast-image'>
+            <img src={data.image} alt='podcast cover' />
+          </div>
+          <div className='podcast-title'>
+            {data.title}
+          </div>
+        </motion.div>
+    )
 }
 
 export default Podcast
