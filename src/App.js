7a6f36da89a36d05e020d28eaf0433fb21f9eccc
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-09-08 23:58:29 -0400

    Add routes to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
 import Podcasts from './components/Podcasts'
 import Home from './components/Home'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Podcasts />
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path='/podcasts'>
+          <Podcasts />
+        </Route>
+      </Switch>
     </div>
   );
 }
