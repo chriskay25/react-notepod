b6dad9188ce36f59c7a7c5c122865219ed132590
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-02-25 13:42:46 -0500

    Add popular podcasts to Home on page load

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import Podcast from "./Podcast";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>WELCOME TO THE NOTEPOD APP!</h1>
-            <Link to='/podcasts'>View Podcasts</Link>
-        </div>
-    )
-}
+  const [popularData, setPopularData] = useState(null);
 
-export default Home
+  const popular = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/v1/search?type=popular"
+      );
+      const data = await response.json();
+      setPopularData(data.podcasts);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const populateHome = async () => {
+      await popular();
+    };
+
+    populateHome();
+  }, []);
+
+  return (
+    <div
+      style={{
+        height: "90%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <div className="scroller">
+        <h3>Popular</h3>
+        <div className="scroller-podcasts">
+          {popularData &&
+            popularData.map((pod) => <Podcast key={pod.id} data={pod} />)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
