4dfa10ea6c7aef4e1d600ecc7c8a44a2fb323252
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-02-28 23:59:51 -0500

    Show podcast info on hover, add more info

--- src/components/Podcast.jsx
+++ src/components/Podcast.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 const Podcast = ({ data }) => {
-    return (
-        <motion.div className='podcast' whileHover={{scale: 1.1}} transition={{ duration: 1 }}>
-          <div className='podcast-image'>
-            <img src={data.image} alt='podcast cover' />
-          </div>
-          <div className='podcast-title'>
-            {data.title}
-          </div>
-        </motion.div>
-    )
-}
+  const [selected, setSelected] = useState(false);
 
-export default Podcast
+  return (
+    <motion.div
+      className="podcast-preview"
+      onMouseEnter={() => setSelected(true)}
+      onMouseLeave={() => setSelected(false)}
+    >
+      <div className="podcast-preview-image">
+        {selected && (
+          <div className="podcast-preview-info">
+            <h2>{data.title}</h2>
+            <div>
+              <span>{data.description}</span>
+            </div>
+          </div>
+        )}
+        {!selected && <img src={data.image} alt="podcast cover" />}
+      </div>
+    </motion.div>
+  );
+};
+
+export default Podcast;
