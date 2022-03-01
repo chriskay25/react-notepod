ec13d267337fd7f0e13380e529bccbefec447aeb
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-02-28 23:56:10 -0500

    Remove routing for now, replace nav w/ title

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import './App.css';
-import { Route, Switch } from 'react-router-dom'
-import Podcasts from './components/Podcasts'
-import Home from './components/Home'
-import NavBar from './components/NavBar'
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path='/podcasts'>
-          <Podcasts />
-        </Route>
-      </Switch>
+      <h1 className="app-title">NOTEPOD</h1>
+      <Home />
     </div>
   );
 }
