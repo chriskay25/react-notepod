f94a7cc560ae74a397fbe4999d7507f2180ce119
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-02-28 23:57:11 -0500

    Create nav component for genre selection

---
+++ src/components/GenreNav.jsx
@@ -0,0 +1,17 @@
+const GenreNav = ({ genres, getNewGenre }) => {
+  return (
+    <nav>
+      <h2 style={{ fontWeight: 400 }}>GENRES</h2>
+      <ul className="genre-list">
+        {genres &&
+          genres.map((genre) => (
+            <li key={genre.id} onClick={() => getNewGenre(genre)}>
+              {genre.name}
+            </li>
+          ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default GenreNav;
