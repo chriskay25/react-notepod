7506fc7a5285b20912a7a49d14b0916f3f7ddb80
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-08 14:27:07 -0500

    Simplify component, remove state

--- src/components/Podcast.jsx
+++ src/components/Podcast.jsx
@@ -1,27 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 
 const Podcast = ({ data }) => {
-  const [selected, setSelected] = useState(false);
-
   return (
-    <motion.div
-      className="podcast-preview"
-      onMouseEnter={() => setSelected(true)}
-      onMouseLeave={() => setSelected(false)}
-    >
+    <motion.li className="podcast-preview">
       <div className="podcast-preview-image">
-        {selected && (
-          <div className="podcast-preview-info">
-            <h2>{data.title}</h2>
-            <div>
-              <span>{data.description}</span>
-            </div>
-          </div>
-        )}
-        {!selected && <img src={data.image} alt="podcast cover" />}
+        <img src={data.image} alt="podcast cover" />
       </div>
-    </motion.div>
+    </motion.li>
   );
 };
 
