6ac3023bb67b99f7e88f15e90518cc49a0064e67
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-08 15:11:47 -0500

    Add container div and move title

--- src/components/Scroller.jsx
+++ src/components/Scroller.jsx
@@ -2,10 +2,16 @@ import Podcast from "./Podcast";
 
 const Scroller = ({ title, data }) => {
   return (
-    <div className="scroller">
-      <h3>{title}</h3>
-      <div className="scroller-podcasts">
-        {data && data.map((pod) => <Podcast key={pod.id} data={pod} />)}
+    <div className="scroller-container">
+      <h3
+        style={{ fontSize: "1.8rem", fontWeight: 300, marginBottom: ".2rem" }}
+      >
+        {title}
+      </h3>
+      <div className="scroller">
+        <div className="scroller-podcasts">
+          {data && data.map((pod) => <Podcast key={pod.id} data={pod} />)}
+        </div>
       </div>
     </div>
   );
