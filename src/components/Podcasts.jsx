ca51f893beedd8c156bf94248c4acfa0d56a1caf
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-08 14:43:39 -0500

    Previous functionality moved to Home, delete most code to start fresh

--- src/components/Podcasts.jsx
+++ src/components/Podcasts.jsx
@@ -1,38 +1,7 @@
-import React, { useState } from 'react'
-import Podcast from './Podcast'
-import { apiKey } from '../secret'
+import React from "react";
 
 const Podcasts = () => {
-    const [podData, setPodData] = useState([])
+  return <div className="podcasts"></div>;
+};
 
-    const handleClick = (e) => {
-        e.preventDefault()
-        fetch("https://listen-api.listennotes.com/api/v2/best_podcasts?genre_id=67&page=2&region=us&safe_mode=1", {
-            method: 'GET',
-            headers: {
-                "X-ListenAPI-Key": apiKey
-            }
-        })
-            .then(resp => resp.json())
-            .then(data => {
-                setPodData(data.podcasts)
-                console.log("data: ", data)
-            })
-            console.log("podData: ", podData)
-    }
-
-    return (
-        <div className='scroller'>
-            <div className='podcasts'>
-                {podData ? podData.map(pod => (
-                    <Podcast key={pod.id} data={pod}/>
-                )): <div>empty</div>}
-            </div>
-            <button onClick={handleClick}>Set Podcasts</button>
-        </div>
-    )
-}
-
-export default Podcasts
-
-
+export default Podcasts;
