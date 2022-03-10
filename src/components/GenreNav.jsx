7bf4ddaa03d511d6411c4fab58cfae489aba001b
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-10 12:48:43 -0500

    Style changes

--- src/components/GenreNav.jsx
+++ src/components/GenreNav.jsx
@@ -1,7 +1,7 @@
 const GenreNav = ({ genres, getNewGenre }) => {
   return (
     <nav className="genre-nav">
-      <h2 style={{ fontWeight: 400 }}>GENRES</h2>
+      <h2>GENRES</h2>
       <ul className="genre-list">
         {genres &&
           genres.map((genre) => (
