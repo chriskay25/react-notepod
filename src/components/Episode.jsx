b4b0cb9522c12b516077e65243157dd44c7a3f41
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-11 17:10:11 -0500

    Flesh out Episode component and add structure

--- src/components/Episode.jsx
+++ src/components/Episode.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const Episode = () => {
+const Episode = ({ episode }) => {
+  const pad2Digits = (digits) => digits.toString().padStart(2, "0");
+
+  const episodeLength = (length) => {
+    const minutes = Math.floor(length / 60);
+    const seconds = length % 60;
+    return `${minutes}:${pad2Digits(seconds)}`;
+  };
+
+  const episodeDate = (timestamp) => {
+    let newDate = new Date(timestamp);
+    return newDate.toLocaleDateString();
+  };
+
   return (
-    <div>
-      Episode
-    </div>
-  )
-}
+    <li
+      className="episode"
+      style={{ margin: "10px 0", borderBottom: "1px solid gray" }}
+    >
+      <h4>{episode.title}</h4>
+      <div style={{ display: "flex", marginBottom: ".5rem" }}>
+        <span>{episodeDate(episode.pub_date_ms)}</span>
+        <span style={{ marginLeft: "1rem" }}>
+          {episodeLength(episode.audio_length_sec)}
+        </span>
+      </div>
+      <p dangerouslySetInnerHTML={{ __html: episode.description }}></p>
+    </li>
+  );
+};
 
-export default Episode
+export default Episode;
