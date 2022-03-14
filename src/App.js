b1b13eac1592ae57356c50132cf8fca877576083
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-13 23:36:57 -0400

    Add a footer w/ required listennotes logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <h1 className="app-title">NOTEPOD</h1>
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import lnLogo from "../assets/listen-notes-logo.png";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <img src={lnLogo} alt="listen-notes-logo" style={{ width: "150px" }} />
+    </div>
+  );
+};
+
+export default Footer;
