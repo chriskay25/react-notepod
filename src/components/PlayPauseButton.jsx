d8201e2b4ac0dbc6335cb00cd876c261c4af6265
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-15 23:57:27 -0400

    Make the svg bigger

--- src/components/PlayPauseButton.jsx
+++ src/components/PlayPauseButton.jsx
@@ -7,12 +7,12 @@ const svgVariants = {
 };
 
 const lineVariant1 = {
-  initial: { x1: -10, y1: 10, x2: -10, y2: 30 },
+  initial: { x1: -20, y1: 20, x2: -20, y2: 60 },
   pause: {
     x1: 0,
-    y1: 0,
+    y1: 4,
     x2: 0,
-    y2: 16,
+    y2: 36,
     strokeWidth: "5px",
     stroke: "white",
   },
@@ -20,39 +20,39 @@ const lineVariant1 = {
     x1: 0,
     y1: 0,
     x2: 0,
-    y2: 20,
+    y2: 40,
     stroke: "white",
   },
 };
 
 const lineVariant2 = {
-  initial: { x1: 22, y1: 10, x2: 22, y2: 30 },
+  initial: { x1: 44, y1: 20, x2: 44, y2: 60 },
   pause: {
-    x1: 12,
-    y1: 0,
-    x2: 12,
-    y2: 20,
+    x1: 24,
+    y1: 4,
+    x2: 24,
+    y2: 36,
     strokeWidth: "5px",
     stroke: "white",
   },
   play: {
     x1: 0,
     y1: 0,
-    x2: 12,
-    y2: 10,
+    x2: 32,
+    y2: 20,
     stroke: "white",
   },
 };
 
 const lineVariant3 = {
-  initial: { opacity: 0, x1: 22, y1: 10, x2: 22, y2: 30 },
+  initial: { opacity: 0, x1: 44, y1: 20, x2: 44, y2: 60 },
   pause: { opacity: 0 },
   play: {
     opacity: 1,
     x1: 0,
-    y1: 20,
-    x2: 12,
-    y2: 10,
+    y1: 40,
+    x2: 32,
+    y2: 20,
     stroke: "white",
   },
 };
@@ -61,8 +61,8 @@ const PlayPauseButton = ({ isPlaying, setIsPlaying }) => {
   return (
     <motion.svg
       className="pause-play-button"
-      height="20px"
-      width="20px"
+      height="40px"
+      width="32px"
       variants={svgVariants}
       initial="initial"
       animate={isPlaying ? "pause" : "play"}
@@ -76,3 +76,80 @@ const PlayPauseButton = ({ isPlaying, setIsPlaying }) => {
 };
 
 export default PlayPauseButton;
+
+// const svgVariants = {
+//   initial: {},
+//   pause: {},
+//   play: {},
+// };
+
+// const lineVariant1 = {
+//   initial: { x1: -10, y1: 10, x2: -10, y2: 30 },
+//   pause: {
+//     x1: 0,
+//     y1: 2,
+//     x2: 0,
+//     y2: 18,
+//     strokeWidth: "5px",
+//     stroke: "white",
+//   },
+//   play: {
+//     x1: 0,
+//     y1: 0,
+//     x2: 0,
+//     y2: 20,
+//     stroke: "white",
+//   },
+// };
+
+// const lineVariant2 = {
+//   initial: { x1: 22, y1: 10, x2: 22, y2: 30 },
+//   pause: {
+//     x1: 12,
+//     y1: 2,
+//     x2: 12,
+//     y2: 18,
+//     strokeWidth: "5px",
+//     stroke: "white",
+//   },
+//   play: {
+//     x1: 0,
+//     y1: 0,
+//     x2: 16,
+//     y2: 10,
+//     stroke: "white",
+//   },
+// };
+
+// const lineVariant3 = {
+//   initial: { opacity: 0, x1: 22, y1: 10, x2: 22, y2: 30 },
+//   pause: { opacity: 0 },
+//   play: {
+//     opacity: 1,
+//     x1: 0,
+//     y1: 20,
+//     x2: 16,
+//     y2: 10,
+//     stroke: "white",
+//   },
+// };
+
+// const PlayPauseButton = ({ isPlaying, setIsPlaying }) => {
+//   return (
+//     <motion.svg
+//       className="pause-play-button"
+//       height="20px"
+//       width="16px"
+//       variants={svgVariants}
+//       initial="initial"
+//       animate={isPlaying ? "pause" : "play"}
+//       onClick={() => setIsPlaying(!isPlaying)}
+//     >
+//       <motion.line variants={lineVariant1} stroke="black" strokeWidth="4px" />
+//       <motion.line variants={lineVariant2} stroke="black" strokeWidth="4px" />
+//       <motion.line variants={lineVariant3} stroke="black" strokeWidth="4px" />
+//     </motion.svg>
+//   );
+// };
+
+// export default PlayPauseButton;
