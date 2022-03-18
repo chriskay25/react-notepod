5df7cc8b4180dfe16e793ec370ef66b308f717db
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-17 23:46:13 -0400

    Pass skip function to JumpButtons

--- src/components/AudioControls.jsx
+++ src/components/AudioControls.jsx
@@ -1,13 +1,13 @@
 import PlayPauseButton from "./PlayPauseButton";
 import JumpButton from "./JumpButton";
 
-const AudioControls = ({ isPlaying, setIsPlaying }) => {
+const AudioControls = ({ isPlaying, setIsPlaying, skip }) => {
   return (
     <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
       <div className="audio-controls">
-        <JumpButton direction="back" />
+        <JumpButton direction="back" skip={skip} />
         <PlayPauseButton isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
-        <JumpButton direction="forward" />
+        <JumpButton direction="forward" skip={skip} />
       </div>
     </div>
   );
