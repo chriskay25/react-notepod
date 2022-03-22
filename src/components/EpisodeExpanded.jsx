13a8dd744c376527f3f2d8fb466105e3254d2f8f
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-22 11:47:09 -0400

    Duplicate some code from Episode for now to flesh out comp

--- src/components/EpisodeExpanded.jsx
+++ src/components/EpisodeExpanded.jsx
@@ -1,7 +1,33 @@
+import AudioPlayer from "./AudioPlayer";
+import { episodeDuration, episodeDate } from "../utils/utils";
+import calendar from "../assets/blue-calendar.png";
+import clock from "../assets/blue-clock.png";
+import close from "../assets/close-icon.png";
+
 const EpisodeExpanded = ({ episode, contractEpisode }) => {
   return (
-    <div className="expanded-episode" onClick={() => contractEpisode()}>
-      <h1>{episode.title}</h1>
+    <div className="episode-expanded">
+      <button
+        className="expand-contract-bttn"
+        onClick={() => contractEpisode()}
+      >
+        <img src={close} alt="close-icon" className="note-icon" />
+      </button>
+      <h2 className="episode-title">{episode.title}</h2>
+      <div className="episode-icons-container">
+        <img className="episode-icon" src={calendar} alt="calendar" />
+        <span>{episodeDate(episode.pub_date_ms)}</span>
+        <img className="episode-icon" src={clock} alt="clock" />
+        <span>{episodeDuration(episode.audio_length_sec)}</span>
+      </div>
+      <div
+        dangerouslySetInnerHTML={{ __html: episode.description }}
+        className="episode-description selected"
+      />
+      <AudioPlayer
+        audioUrl={episode.audio}
+        audioDuration={episodeDuration(episode.audio_length_sec)}
+      />
     </div>
   );
 };
