2a727d441c34d2b0ac48b4cc4567b28454a940d9
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-23 23:15:36 -0400

    Specify payload contents, add an action to set genre state

--- src/actions/podcasts.js
+++ src/actions/podcasts.js
@@ -4,7 +4,6 @@ export const getPodcasts = () => async (dispatch) => {
       "http://localhost:3000/api/v1/search?type=popular"
     );
     const data = await response.json();
-    console.log("Result of getPodcasts: ", data);
     dispatch({ type: "GET_PODCASTS", payload: data.podcasts });
   } catch (err) {
     console.log(err);
@@ -23,13 +22,16 @@ export const getGenres = () => async (dispatch) => {
   }
 };
 
-export const getPodcastsByGenre = (genreId) => async (dispatch) => {
+export const getPodcastsByGenre = (genre) => async (dispatch) => {
   try {
     const response = await fetch(
-      `http://localhost:3000/api/v1/search?type=popular&genre_id=${genreId}`
+      `http://localhost:3000/api/v1/search?type=popular&genre_id=${genre.id}`
     );
     const data = await response.json();
-    dispatch({ type: "GET_GENRES", payload: data.podcasts });
+    dispatch({
+      type: "GET_PODCASTS_BY_GENRE",
+      payload: { podcasts: data.podcasts, genre: genre },
+    });
   } catch (err) {
     console.log(err);
   }
@@ -45,4 +47,10 @@ export const getPodcast = (podcastId) => async (dispatch) => {
   } catch (err) {
     console.log(err);
   }
+};
+
+export const selectedGenre = (genre) => {
+  return (dispatch) => {
+    dispatch({ type: "SELECTED_GENRE", payload: genre });
+  };
 };
