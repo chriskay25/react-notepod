543cee46e4362587ab0968f04395870f0cd287a7
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-23 23:05:19 -0400

    Dispatch action to get podcasts by genre id on click

--- src/components/GenreNav.jsx
+++ src/components/GenreNav.jsx
@@ -1,13 +1,19 @@
 import Footer from "./Footer";
+import { useDispatch } from "react-redux";
+import { getPodcastsByGenre } from "../actions/podcasts";
 
-const GenreNav = ({ genres, getNewGenre }) => {
+const GenreNav = ({ genres }) => {
+  const dispatch = useDispatch();
   return (
     <nav className="genre-nav">
       <h2>GENRES</h2>
       <ul className="genre-list">
         {genres &&
           genres.map((genre) => (
-            <li key={genre.id} onClick={() => getNewGenre(genre)}>
+            <li
+              key={genre.id}
+              onClick={() => dispatch(getPodcastsByGenre(genre))}
+            >
               {genre.name}
             </li>
           ))}
