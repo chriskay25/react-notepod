022b76baa517d783533cb78c8d874a437e7dcdb2
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-23 23:07:28 -0400

    Dispatch action to get selected podcast onClick

--- src/components/PodcastPreview.jsx
+++ src/components/PodcastPreview.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { motion } from "framer-motion";
+import { getPodcast } from "../actions/podcasts";
 
-const PodcastPreview = ({ data, getPodcast }) => {
+const PodcastPreview = ({ data }) => {
+  const dispatch = useDispatch();
+
   return (
     <motion.li className="podcast-preview">
       <div
         className="podcast-preview-image"
-        onClick={() => getPodcast(data.id)}
+        onClick={() => dispatch(getPodcast(data.id))}
       >
         <img src={data.image} alt="podcast cover" />
       </div>
