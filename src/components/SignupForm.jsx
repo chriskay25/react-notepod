a468edcdfcebe6df336d19735f3b984b3f90de20
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-28 22:27:31 -0400

    Add a Signup component

---
+++ src/components/SignupForm.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { signup } from "../actions/users";
+
+const SignupForm = () => {
+  const dispatch = useDispatch();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleNameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePwChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(signup({ username: username, password: password }));
+  };
+
+  return (
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={username}
+          onChange={handleNameChange}
+        />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={handlePwChange}
+        />
+      </div>
+      <input type="submit" value="Sign Up" />
+    </form>
+  );
+};
+
+export default SignupForm;
