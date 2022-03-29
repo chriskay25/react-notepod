6754f727ed866b6e5fc91aa04270ca5562bab2a8
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-28 22:29:17 -0400

    Check for JWT when app loads and get correct user accordingly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import AuthForms from "./components/AuthForms";
+import { getCurrentUser } from "./actions/users";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.currentUser);
+
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    if (!currentUser && token) {
+      dispatch(getCurrentUser());
+    }
+  }, [dispatch, currentUser]);
+
   return (
     <div className="App">
       <Header />
-      <Home />
+      {currentUser ? <Home /> : <AuthForms />}
     </div>
   );
-}
+};
 
 export default App;
