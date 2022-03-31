29e97fff7d04e123a9c7ee85fb47c1f5ac70e98b
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-31 01:05:12 -0400

    Remove unnecessary tags

--- src/components/AudioControls.jsx
+++ src/components/AudioControls.jsx
@@ -3,12 +3,10 @@ import JumpButton from "./JumpButton";
 
 const AudioControls = ({ isPlaying, setIsPlaying, skip }) => {
   return (
-    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-      <div className="audio-controls">
-        <JumpButton direction="back" skip={skip} />
-        <PlayPauseButton isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
-        <JumpButton direction="forward" skip={skip} />
-      </div>
+    <div className="audio-controls">
+      <JumpButton direction="back" skip={skip} />
+      <PlayPauseButton isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
+      <JumpButton direction="forward" skip={skip} />
     </div>
   );
 };
