d12d1c9c839c558420bb10aa9835b3a7a6972577
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-01 15:12:50 -0400

    Get and set state for nav menu from this component

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,20 +1,26 @@
-import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { menuToggle } from "../actions/menu";
 
 const Hamburger = () => {
-  const [menuOpen, toggle] = useState(false);
+  const dispatch = useDispatch();
+  const menuIsOpen = useSelector((state) => state.menuIsOpen);
+
+  const toggleMenu = (bool) => {
+    dispatch(menuToggle(bool));
+  };
 
   return (
-    <span className="menu-hamburger" onClick={() => toggle(!menuOpen)}>
+    <span className="menu-hamburger" onClick={() => toggleMenu(!menuIsOpen)}>
       <svg
         width="40px"
         height="40px"
         strokeWidth="2px"
-        stroke={menuOpen ? "#000" : "#fff"}
+        stroke={menuIsOpen ? "#000" : "#fff"}
         strokeLinecap="round"
       >
-        <path d={menuOpen ? "m 0 10, 25 20" : "m 0 10, 25 0"} />
-        <path d="m 0 18, 25 0" style={{ opacity: menuOpen ? 0 : 1 }} />
-        <path d={menuOpen ? "m 0 30, 25 -20" : "m 0 26, 25 0"} />
+        <path d={menuIsOpen ? "m 0 10, 25 20" : "m 0 10, 25 0"} />
+        <path d="m 0 18, 25 0" style={{ opacity: menuIsOpen ? 0 : 1 }} />
+        <path d={menuIsOpen ? "m 0 30, 25 -20" : "m 0 26, 25 0"} />
       </svg>
     </span>
   );
