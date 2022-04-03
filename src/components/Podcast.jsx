f6163cb44b2366c4b341a737202a62b5e3f366d5
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-03 16:18:56 -0400

    Simplify component

--- src/components/Podcast.jsx
+++ src/components/Podcast.jsx
@@ -1,45 +1,22 @@
-import React, { useState } from "react";
-import Episode from "./Episode";
-import EpisodeExpanded from "./EpisodeExpanded";
+import EpisodePreview from "./EpisodePreview";
 
 const Podcast = ({ podcast }) => {
-  const [episode, setEpisode] = useState(null);
-
-  const expandEpisode = (e) => {
-    setEpisode(e);
-  };
-
-  const contractEpisode = () => {
-    setEpisode(null);
-  };
-
   return (
     <div className="podcast">
-      {episode && (
-        <EpisodeExpanded episode={episode} contractEpisode={contractEpisode} />
-      )}
-      {!episode && (
-        <>
-          <div className="podcast-title-grid">
-            <div className="podcast-title-image">
-              <img src={podcast.thumbnail} alt="podcast-thumbnail" />
-            </div>
-            <h2 className="podcast-title">{podcast.title}</h2>
-            <p className="podcast-description">{podcast.description}</p>
-          </div>
-          <div>
-            <ul>
-              {podcast.episodes.map((episode) => (
-                <Episode
-                  key={episode.id}
-                  episode={episode}
-                  expandEpisode={expandEpisode}
-                />
-              ))}
-            </ul>
-          </div>
-        </>
-      )}
+      <div className="podcast-title-grid">
+        <div className="podcast-title-image">
+          <img src={podcast.thumbnail} alt="podcast-thumbnail" />
+        </div>
+        <h2 className="podcast-title">{podcast.title}</h2>
+        <p className="podcast-description">{podcast.description}</p>
+      </div>
+      <div>
+        <ul>
+          {podcast.episodes.map((episode) => (
+            <EpisodePreview key={episode.id} episode={episode} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
