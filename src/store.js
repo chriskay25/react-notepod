1bd6111ceb0f365a12f3db8b0735f7d7a8f02376
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-03 15:46:58 -0400

    Persist and retrieve state from local storage

--- src/store.js
+++ src/store.js
@@ -5,5 +5,32 @@ import rootReducer from "./reducers/rootReducer";
 const composeEnhancers =
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) || compose;
 
-let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const saveStateToLocalStorage = (state) => {
+  try {
+    localStorage.setItem("state", JSON.stringify(state));
+  } catch (err) {
+    console.log("Error saving state to local storage: ", err);
+  }
+};
+
+const getStateFromLocalStorage = () => {
+  try {
+    const state = localStorage.getItem("state");
+    return state ? JSON.parse(state) : undefined;
+  } catch (err) {
+    console.log("Error retrieving state from local storage: ", err);
+  }
+};
+
+let localState = getStateFromLocalStorage();
+
+let store = createStore(
+  rootReducer,
+  localState,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+store.subscribe(() => {
+  saveStateToLocalStorage(store.getState());
+});
 export default store;
