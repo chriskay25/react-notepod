cab51bcd2fb7b8ded027705ae9ae8132df55c67d
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-09 13:36:56 -0400

    Import EpisodeCenter, use ref to help set up slide action

--- src/components/Episode.jsx
+++ src/components/Episode.jsx
@@ -1,23 +1,25 @@
+import React, { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { episodeDuration } from "../utils/utils";
 import EpisodeHeader from "./EpisodeHeader";
-import EpisodeDescription from "./EpisodeDescription";
 import AudioPlayer from "./AudioPlayer";
+import EpisodeCenter from "./EpisodeCenter";
 import { selectEpisode } from "../actions/episodes";
 import close from "../assets/close-icon.png";
 const Episode = ({ episode }) => {
+  const episodeRef = useRef();
   const dispatch = useDispatch();
 
   return (
-    <div className="episode">
+    <div ref={episodeRef} className="episode">
       <button
         className="expand-contract-bttn"
         onClick={() => dispatch(selectEpisode(null))}
       >
         <img src={close} alt="close-icon" className="note-icon" />
       </button>
-      <EpisodeHeader episode={episode} />
-      <EpisodeDescription description={episode.description} />
+      <EpisodeHeader episode={episode} noteMode />
+      <EpisodeCenter ref={episodeRef} description={episode.description} />
       <AudioPlayer
         audioUrl={episode.audio}
         audioDuration={episodeDuration(episode.audio_length_sec)}
