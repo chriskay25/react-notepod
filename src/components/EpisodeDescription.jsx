a314e4a3442e013a9d44352ac87388afe23bbd30
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-09 13:24:27 -0400

    Add a header, also new conditional class for notetaking mode

--- src/components/EpisodeDescription.jsx
+++ src/components/EpisodeDescription.jsx
@@ -1,21 +1,31 @@
+import { motion } from "framer-motion";
+
 const EpisodeDescription = ({
   description,
   expanded,
   setExpanded,
   preview,
+  noteMode,
 }) => {
   const handleClick = () => {
     if (preview) setExpanded(!expanded);
   };
 
   return (
-    <div
-      dangerouslySetInnerHTML={{ __html: description }}
-      className={`episode-description ${preview ? "preview" : ""} ${
-        expanded ? "expanded" : ""
-      }`}
-      onClick={() => handleClick()}
-    />
+    <div style={{ height: "100%", overflow: "hidden" }}>
+      {noteMode && (
+        <h3 style={{ fontWeight: 500, borderBottom: "2px solid white" }}>
+          Description{" "}
+        </h3>
+      )}
+      <motion.div
+        dangerouslySetInnerHTML={{ __html: description }}
+        className={`episode-description ${preview ? "preview" : ""} ${
+          expanded ? "expanded" : ""
+        } ${noteMode ? "note-mode" : ""}`}
+        onClick={() => handleClick()}
+      ></motion.div>
+    </div>
   );
 };
 
