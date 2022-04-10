e0029d11b93481f136a45c57224d44bc5150c563
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-10 15:45:39 -0400

    Check localStorage for genres to avoid unnecessary API calls

--- src/actions/genres.js
+++ src/actions/genres.js
@@ -1,12 +1,22 @@
 export const getGenres = () => async (dispatch) => {
-  try {
-    const response = await fetch(
-      "http://localhost:3000/api/v1/search?type=genres"
-    );
-    const data = await response.json();
-    dispatch({ type: "GET_GENRES", payload: data.genres });
-  } catch (err) {
-    console.log(err);
+  // If genres in local storage, set state from there instead of making API req
+  let state = localStorage.getItem("state");
+  let parsedState = state ? JSON.parse(state) : "";
+  if (parsedState && parsedState.genreReducer.genres.length > 0) {
+    dispatch({
+      type: "GET_GENRES",
+      payload: parsedState.genreReducer.genres,
+    });
+  } else {
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/v1/search?type=genres"
+      );
+      const data = await response.json();
+      dispatch({ type: "GET_GENRES", payload: data.genres });
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 
