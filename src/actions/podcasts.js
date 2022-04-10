8daa042606bacc409d89a28124195525300452e9
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-10 15:45:58 -0400

    Check localStorage for podcasts to avoid unnecessary API calls

--- src/actions/podcasts.js
+++ src/actions/podcasts.js
@@ -1,12 +1,22 @@
 export const getPodcasts = () => async (dispatch) => {
-  try {
-    const response = await fetch(
-      "http://localhost:3000/api/v1/search?type=popular"
-    );
-    const data = await response.json();
-    dispatch({ type: "GET_PODCASTS", payload: data.podcasts });
-  } catch (err) {
-    console.log(err);
+  // If podcasts in local storage, set state from there instead of making API req
+  let state = localStorage.getItem("state");
+  let parsedState = state ? JSON.parse(state) : "";
+  if (parsedState && parsedState.podcastReducer.podcasts.length > 0) {
+    dispatch({
+      type: "GET_PODCASTS",
+      payload: parsedState.podcastReducer.podcasts,
+    });
+  } else {
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/v1/search?type=popular"
+      );
+      const data = await response.json();
+      dispatch({ type: "GET_PODCASTS", payload: data.podcasts });
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 
