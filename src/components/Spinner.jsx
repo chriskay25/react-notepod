1d1c470119ea863f1d8d3dabec55e6135e096b50
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-20 23:57:09 -0400

    Change spinner to more of a wave

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,20 +1,47 @@
+import { motion } from "framer-motion";
+
+const spinner = {
+  initial: {},
+  animate: { transition: { staggerChildren: 0.1 } },
+};
+
+const wave = {
+  initial: { scale: 0.1 },
+  animate: {
+    scale: 1,
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 0.8,
+      ease: "circInOut",
+    },
+  },
+};
+
 const Spinner = () => {
   return (
-    <div
+    <motion.div
+      layout
+      initial="initial"
+      animate="animate"
+      variants={spinner}
       style={{
         height: "100%",
         width: "100%",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        background: "#262626",
+        padding: "10px 0",
       }}
     >
-      <div className="spinner">
-        LOADING
-        <span></span>
-      </div>
-    </div>
+      <motion.div layout className="wave" variants={wave} />
+      <motion.div layout className="wave" variants={wave} />
+      <motion.div layout className="wave" variants={wave} />
+      <motion.div layout className="wave" variants={wave} />
+      <motion.div layout className="wave" variants={wave} />
+      <motion.div layout className="wave" variants={wave} />
+      <motion.div layout className="wave" variants={wave} />
+    </motion.div>
   );
 };
 
