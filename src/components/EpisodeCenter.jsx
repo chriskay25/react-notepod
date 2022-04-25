84159d6b57b4750d0f425122e195a339d1822138
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-24 23:59:07 -0400

    Delete extracted header code, change comp behavior

--- src/components/EpisodeCenter.jsx
+++ src/components/EpisodeCenter.jsx
@@ -1,95 +1,49 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import EpisodeDescription from "./EpisodeDescription";
 import Notepad from "./Notepad";
-import { motion, useAnimation, useMotionValue } from "framer-motion";
+import EpisodeCenterHeader from "./EpisodeCenterHeader";
+import { motion, AnimatePresence, useCycle } from "framer-motion";
 
-const EpisodeCenter = React.forwardRef((props, ref) => {
-  const [width, setWidth] = useState(100);
-  const x = useMotionValue(0);
-  const controls = useAnimation();
-
-  const handleDragEnd = async (e, info) => {
-    const offset = info.offset.x;
-    if (offset < -100) {
-      controls.start({ x: -width, transition: { duration: 0.2 } });
-    }
-    if (offset > 100) {
-      controls.start({ x: 0, opacity: 1, transition: { duration: 0.2 } });
-    }
-  };
-
-  useEffect(() => {
-    const resizeObserver = new ResizeObserver((e) => {
-      setWidth(e[0].contentBoxSize[0].inlineSize);
-    });
-    if (ref && ref.current) {
-      resizeObserver.observe(ref.current);
-    }
-  }, [ref]);
+const EpisodeCenter = ({ newNote, description }) => {
+  const [element, cycleElement] = useCycle("DESCRIPTION", "NOTEPAD");
+  const [showForm, setShowForm] = useState(true);
 
   return (
-    <motion.div
-      className="episode-center"
-      ref={ref}
-      drag="x"
-      dragConstraints={{ right: 0, left: 0 }}
-      dragTransition={{ bounceStiffness: 600, bounceDamping: 30 }}
-      dragElastic={0.5}
-      whileHover={{ cursor: "pointer" }}
-      whileTap={{ cursor: "grabbing" }}
-      whileDrag={{ cursor: "grabbing" }}
-      onDragEnd={(e, info) => handleDragEnd(e, info)}
-      style={{ x }}
-      animate={controls}
-      layout
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: width,
-          overflow: "hidden",
-        }}
-      >
-        <div
+    <motion.div className="episode-center">
+      <EpisodeCenterHeader
+        element={element}
+        cycleElement={cycleElement}
+        showForm={showForm}
+        setShowForm={setShowForm}
+      />
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          layout
+          key={element}
+          initial={{ y: "-75px", scale: 0 }}
+          animate={{ y: 0, scale: 1, transition: { duration: 0.2 } }}
+          exit={{ y: "100px", scale: 0, transition: { duration: 0.2 } }}
           style={{
-            height: "95%",
-            background: "var(--gray-text)",
-            color: "white",
-            borderRadius: "5px",
-            padding: "20px 20px",
-            overflow: "hidden",
-          }}
-        >
-          <EpisodeDescription description={props.description} noteMode />
-        </div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: width - 5,
-          overflow: "hidden",
-        }}
-      >
-        <div
-          style={{
-            height: "95%",
+            height: "calc(100% - 50px)",
             width: "100%",
-            background: "var(--gray-text)",
-            color: "white",
-            borderRadius: "5px",
-            padding: "20px 20px",
+            padding: "10px",
+            color: "var(--gray-text)",
             overflow: "hidden",
           }}
         >
-          <Notepad newNote={props.newNote} />
-        </div>
-      </div>
+          {element === "DESCRIPTION" ? (
+            <EpisodeDescription description={description} noteMode />
+          ) : (
+            <Notepad
+              newNote={newNote}
+              showForm={showForm}
+              setShowForm={setShowForm}
+            />
+          )}
+        </motion.div>
+      </AnimatePresence>
     </motion.div>
   );
-});
+};
 
 export default EpisodeCenter;
