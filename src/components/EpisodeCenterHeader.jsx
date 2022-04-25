ac0ac2b7f9004d47087582a2176c81dcabc66903
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-24 23:58:11 -0400

    Add a component for the EpisodeCenter header

---
+++ src/components/EpisodeCenterHeader.jsx
@@ -0,0 +1,76 @@
+import { motion } from "framer-motion";
+import swapIcon from "../assets/swap-icon-4.png";
+
+const EpisodeCenterHeader = ({
+  element,
+  cycleElement,
+  showForm,
+  setShowForm,
+}) => {
+  return (
+    <motion.header
+      layout
+      style={{
+        position: "relative",
+        marginTop: "10px",
+        padding: "0 10px",
+        height: "3rem",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderBottom: "2px solid var(--gray-text)",
+      }}
+    >
+      <motion.h2
+        layout
+        style={{
+          color: "var(--gray-text)",
+          fontWeight: 500,
+          fontSize: "2.2rem",
+          lineHeight: "2.2rem",
+        }}
+      >
+        {element}
+      </motion.h2>
+      <motion.div style={{ display: "flex", height: "2.2rem" }}>
+        {element === "NOTEPAD" && (
+          <button
+            className="notepad-buttons"
+            onClick={() => setShowForm(!showForm)}
+          >
+            {showForm ? "View Notes" : "New Note"}
+          </button>
+        )}
+        <motion.button
+          layout
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "2.2rem",
+            width: "2.2rem",
+            marginLeft: "8px",
+            cursor: "pointer",
+            borderRadius: "50%",
+            padding: ".4rem",
+            background: "black",
+          }}
+          whileHover={{
+            background: "var(--gray-text)",
+            transition: { duration: 0.3 },
+          }}
+        >
+          <motion.img
+            style={{ width: "100%" }}
+            layout
+            src={swapIcon}
+            alt="info-icon"
+            onClick={() => cycleElement()}
+          />
+        </motion.button>
+      </motion.div>
+    </motion.header>
+  );
+};
+
+export default EpisodeCenterHeader;
